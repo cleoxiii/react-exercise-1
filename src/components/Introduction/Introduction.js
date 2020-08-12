f48c2e2ab0d097bb391ea5fb8c3e0966ff068ccf
Yun Chenxi <cleoxiii@163.com>
2020-08-12 11:44:16 +0800

    [yunchenxi] refactor: use another way to set img src.

--- src/components/Introduction/Introduction.js
+++ src/components/Introduction/Introduction.js
@@ -1,12 +1,12 @@
-/* eslint-disable no-undef */
 import React from 'react';
 import './Introduction.scss';
+import imgURL from '../../assets/avatar.jpg';
 
 class Introduction extends React.Component {
   render() {
     return (
       <div className="introduction">
-        <img src={require('../../assets/avatar.jpg')} alt="profile" />
+        <img src={imgURL} alt="profile" />
         <h1>HELLO,</h1>
         <h2>MY NAME IS KAMIL 24YO AND THIS IS MY RESUME/CV</h2>
       </div>
